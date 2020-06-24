348bf46885bfbca667cb2ea7611d13542ceb60e3
godfreydlamini <zwescientist@gmail.com>
2020-06-24 19:25:29 +0200

    added validateInput function, multiple delimiters still failing

--- stringCalculator.js
+++ stringCalculator.js
@@ -21,9 +21,40 @@ function getDelimNumbers(stringInput) {
     return delimeterNumbers;
 }
 
+function validateInput(stringInput) {
+    let lastChar = stringInput[stringInput.length - 1];
+    let lastCharCode = lastChar.charCodeAt(0);
+    if (!(lastCharCode >= 48 && lastCharCode <= 57)) {
+        throw "ERROR: invalid input";
+    }
+    if (stringInput.indexOf("//") != -1 && stringInput.indexOf("//") != 0) {
+        throw "ERROR: invalid input";
+    }
+    //build a string for negative numbers
+    let negatives = "";
+    let negValues = stringInput.match(/-\d+/g);
+    if (negValues != null) {
+        for (let i = 0; i < negValues.length; i++) {
+            let number = negValues[i];
+            if (i != negValues.length - 1) {
+                negatives += number + ", ";
+            } else {
+                negatives += number;
+            }
+        }
+        throw `ERROR: negatives not allowed ${negatives}`;
+    }
+
+}
+
 function add(stringInput) {
+    if (stringInput.length == 0) {
+        return 0;
+    }
+    validateInput(stringInput);
     let finalNumbers = [];
     let delimiterNumbers = getDelimNumbers(stringInput);
+    console.log(delimiterNumbers);
     if (delimiterNumbers == null) {
         numbers = stringInput.match(numbersRegex).map(Number);
         let total = sumNumbers(numbers);
@@ -34,6 +65,7 @@ function add(stringInput) {
             let n = numbers[i];
             for (let pos = 0; pos < delimiterNumbers.length; pos++) {
                 let currentDelimNumber = delimiterNumbers[pos];
+                console.log(currentDelimNumber);
                 if (n != currentDelimNumber) {
                     let nSplit = n.split(currentDelimNumber);
                     for (let j = 0; j < nSplit.length; j++) {
@@ -49,4 +81,4 @@ function add(stringInput) {
 }
 
 
-console.log(add("1,1"));
+console.log(add("//4\n142"));
